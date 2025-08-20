39ca4f57249082759ad53c9b53fe5ee369047ef7
Bryan-Escobar <bryan.escobar2@catolica.edu.sv>
2025-08-20 17:51:02 -0600

    arregle el script

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
 const cancelarReserva = (event) => {
+    const btnCancelar = event.currentTarget;
 
+    btnCancelar.parentElement.parentElement.remove()
 }
 
 const calcularNumeroNoches = (fechaEntrada, fechaSalida) => {
@@ -11,8 +13,19 @@ const calcularNumeroNoches = (fechaEntrada, fechaSalida) => {
     return totalNoches;
 }
 
+const mostrarEstadisticas = (nNoches, total) => {
+    const txtNumeroNoches = document.getElementById("txtNumeroNoches");
+
+    const txtTotalReserva = document.getElementById("txtTotalReserva")
+
+    txtNumeroNoches.innerText = "Noches: " + nNoches;
+    txtTotalReserva.innerText = "Total: " + total;
+}
+
 const agregarReserva = (event) => {
 
+
+    //establece precio por defecto
     const PRECIO = 50;
 
     const nombre = event.inputNombre.value;
@@ -30,8 +43,46 @@ const agregarReserva = (event) => {
 
     const listaReservas = document.getElementById("listaReservas");
 
+    console.log(`${nombre}   ${email}   ${telefono}   ${fechaEntrada}  ${fechaSalida}  ${nPersonas}`)
 
+    //añadimos reserva
+
+    listaReservas.innerHTML += `<div class="card card-body container d-flex" id="tarjetaReserva">
+
+                <div class="row align-items-center">
+
+                    <div class="col-12 col-sm-10">
+                        <div class="d-flex align-items-center" style="gap: 10px;">
+                            <h4 class="fw-medium fs-4" id="nombreUsuario">${nombre}</h4>
+                            <h5 class="fw-light fs-5" id="correoUsuario">${email}</h5>
+                            <h5 class="fw-light fs-5">|</h5>
+                            <h5 class="fw-light" id="telefonoUsuario">${telefono}</h5>
+                        </div>
+
+                        <div class="card-subtitle d-flex align-items-center" style="gap: 10px;">
+                            <h5 class="fw-light fs-5" id="fechaEntrada">${fechaEntrada}</h5>
+                            <h5 class="fw-light fs-5">=></h5>
+                            <h5 class="fw-light fs-5" id="fechaSalida">${fechaSalida}</h5>
+
+                            <h5 class="fw-light fs-5" id="personas">personas: ${nPersonas}</h5>
+                            <h5 class="fw-light fs-5">|</h5>
+                            <h5 class="fw-light" id="noches">noches: ${nNoches}</h5>
+                        </div>
+
+                        <div class="card-subtitle d-flex align-items-center" style="gap: 10px;">
+                            <h5 class="fw-bold fs-5" id="fechaEntrada">Total: $</h5>
+                            <h5 class="fw-bold fs-5">400</h5>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-sm-2">
+                        <button class="btn btn-danger" id="btnCancelar">Cancelar</button>
+                    </div>
+                </div>
+            </div>`
 }
+
+
 
 
 
